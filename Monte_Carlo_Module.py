f9624b12857d54bbc58e5c5c39311145613f3dd3
DYZhang117 <diyanz2@illinois.edu>
2021-11-28 17:32:24 -0600

    push num_assign()

--- Monte_Carlo_Module.py
+++ Monte_Carlo_Module.py
@@ -5,7 +5,40 @@ import pandas as pd
 import math
 # import statistics as st
 
-def workday_OR_weekend(max_resident, day):
+def num_assign(num_resident):
+    # frequency = k 执行k次num_assign();
+    # according to "readme.md" -- The orchard downs contains almost same number of 2B2B and 1B1B. About 40% of 2B2B
+    # tenants are families (more than 3 people), about 20% are one person, and about 40% are two people. For 1B1B,
+    # about 50% are two people living, and about 50% are living alone.
+
+    num_2b2b = math.floor(num_resident/2)
+    num_1b1b = num_resident - num_2b2b
+
+    family = math.floor(0.4 * num_2b2b)
+    solitude = math.floor(0.4 * num_2b2b + 0.5 * num_1b1b)
+    couple = num_resident - family - solitude
+
+    # 因为人们有更大的概率在周末洗衣服，假设在周末洗衣服概率为0.7，在工作日洗衣服概率为0.3
+    # 假设在周末 人们洗衣服的人数 -- 0 代表在周末洗衣服；1 代表在工作日洗衣服
+    num_family_weekday, num_solitude_weekday, num_couple_weekday = \
+        np.random.binomial(family,0.3), np.random.binomial(solitude,0.3), np.random.binomial(couple,0.3) # 在工作日洗衣服的人数
+    num_family_weekend, num_solitude_weekend, num_couple_weekend = \
+        family - num_family_weekday, family - num_solitude_weekday, family - num_couple_weekday # 在周末洗衣服的人数
+    # return num_family_weekday, num_family_weekend, num_solitude_weekday, num_solitude_weekend, num_couple_weekday, num_couple_weekend
+
+    # 随机分配 具体洗衣时间 工作日（周一~周五）， 周末（周六~周日）
+    family_weekday_assign = np.random.randint(1, 6, size=num_family_weekday)
+    solitude_weekday_assign = np.random.randint(1, 6, size=num_solitude_weekday)
+    couple_weekday_assign = np.random.randint(1, 6, size=num_couple_weekday)
+    family_weekend_assign = np.random.randint(1, 6, size=num_family_weekend)
+    solitude_weekend_assign = np.random.randint(1, 6, size=num_solitude_weekend)
+    couple_weekend_assign = np.random.randint(1, 6, size=num_couple_weekend)
+
+    return family_weekday_assign, solitude_weekday_assign, couple_weekday_assign, family_weekend_assign, solitude_weekend_assign, couple_weekend_assign
+
+def workday_OR_weekend(num_resident, day, frequency=1):
+
+    # 首先分配
 
     # only consider the cases in a week
     workday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
@@ -16,7 +49,7 @@ def workday_OR_weekend(max_resident, day):
 
     if day in workday:
         # today is workday, and residents
-        pass
+        random_value = []
 
 # code reference https://github.com/Zainabzav/final_projects/blob/master/montecarlo_module.py
 class Laundry:
@@ -24,20 +57,20 @@ class Laundry:
     This class contains the basic and initial information of washing machines and dryers of the laundry.
     We only consider the operation of the laundry room for a week, including five working days and a weekends.
     '''
-    def __init__(self, num_WashMachine, num_Dryer, max_resident, time_interval, washTime, dryTime, frequency):
+    def __init__(self, num_WashMachine, num_Dryer, num_resident, time_interval, washTime, dryTime, frequency):
         '''
         This function assigns the initial value for the attributes.
         :param num_WashMachine: number of washing machines
         :param num_Dryer: number of dryers
-        :param max_resident: maximum number of residents
+        :param num_resident: number of residents
         :param time_interval: The time to wait for the user to take out the clothes each time the washing machine or dryer finishes its work (0 min< time< 15 mins)
         :param washTime: Each washing time of the washing machine (25 mins<time<70 mins)
         :param dryTime: Each working time of the dryer (35 mins<time<140 mins)
-        :param frequency: The washing frequency of each unit.
+        :param frequency: The washing frequencnum_residenty of each unit.
         '''
         self.num_WashMachine = num_WashMachine
         self.num_Dryer = num_Dryer
-        self.max_resident = max_resident
+        self.num_resident = num_resident
         self.time_interval = time_interval
         self.washTime = washTime
         self.dryTime = dryTime
@@ -51,20 +84,20 @@ class Laundry:
         '''
         num_WashMachine = int(input("The Number of washing machines (Integer greater than 0):"))
         num_Dryer = int(input("The Number of dryers (Integer greater than 0):"))
-        max_resident = int(input("The Maximum Number of resident (greater than 0):"))
+        num_resident = int(input("The Number of resident (greater than 0):"))
         time_interval = input("The time to wait for the user to take out the clothes each time the washing machine or dryer finishes its work (0 min< time< 15 mins):")
         washTime = input("Each washing time of the washing machine (25 mins<time<70 mins):")
         dryTime = input("Each working time of the dryer (35 mins<time<140 mins):")
         frequency = input("The washing frequency of each unit (* times/a week):")
 
-        return cls(num_WashMachine, num_Dryer, max_resident, time_interval, washTime, dryTime, frequency)
+        return cls(num_WashMachine, num_Dryer, num_resident, time_interval, washTime, dryTime, frequency)
 
-    def random_simulation(self, num_WashMachine, num_Dryer, max_resident, time_interval, washTime, dryTime, frequency):
+    def random_simulation(self, num_WashMachine, num_Dryer, num_resident, time_interval, washTime, dryTime, frequency):
         '''
         This function creat random variables to simulates the operation of the laundry within a week.
         :param num_WashMachine:
         :param num_Dryer:
-        :param max_resident:
+        :param num_resident:
         :param time_interval:
         :param washTime:
         :param dryTime:
