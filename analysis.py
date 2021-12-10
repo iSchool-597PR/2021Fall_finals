e0b276b7dbaf336ca03011fdc6454c58c2251074
prayag99 <prayagpatel1999@gmail.com>
2021-12-10 14:32:23 -0600

    analysis updated function

--- analysis.py
+++ analysis.py
@@ -17,3 +17,14 @@ def read_file(year, filename):
         df = pd.read_csv(file, encoding='unicode_escape')
 
     return df
+
+def read_file_excel(year, filename, head = 0):
+    dataframes_dict = {}
+    file = "data/" + str(year) + "/" + str(filename) + ".xlsx"
+    # file1 = "data/" + str(i) + "/ACCIDENT.csv"
+    try:
+        df = pd.read_excel(file, header = head)
+    except:
+        df = pd.read_excel(file, header = head, encoding='unicode_escape')
+
+    return df
