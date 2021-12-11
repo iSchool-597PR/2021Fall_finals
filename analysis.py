ce7e60147283afe1c011782b71b837eb87cad91c
prayag99 <prayagpatel1999@gmail.com>
2021-12-10 19:54:22 -0600

    Added more functions and modified Visualizations.ipynb

--- analysis.py
+++ analysis.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import xlrd
-# import openpyxl
-# import geopandas
-# from shapely import wkt
+import openpyxl
+import geopandas
+from shapely import wkt
 
 def read_file(year, filename):
     dataframes_dict = {}
@@ -28,3 +28,66 @@ def read_file_excel(year, filename, head = 0):
         df = pd.read_excel(file, header = head, encoding='unicode_escape')
 
     return df
+
+
+def values(df):
+    total_killed = {}
+    for index, row in df.iterrows():
+        if ((row['Vehicle Type'] != 'Total') and (row['Person Type'] == 'Subtotal')):
+            total_killed[row['Vehicle Type']] = row['Occupants Killed']
+        elif ((row['Vehicle Type'] == 'Motorcyclists' or row['Vehicle Type'] == 'Nonoccupants') and (
+                row['Person Type'] == 'Total')):
+            total_killed[row['Vehicle Type']] = row['Occupants Killed']
+        else:
+            continue
+    return total_killed
+
+
+def plotpie(dict1, year):
+    labels = []
+    number = []
+    percent = []
+    plt.figure(1, figsize=(7, 7))
+
+    # Referred from
+    # https://stackoverflow.com/questions/62593913/plotting-a-pie-chart-out-of-a-dictionary
+    for x, y in dict1.items():
+        labels.append(x)
+        number.append(y)
+    plt.pie(number, labels=labels, startangle=90, autopct='%1.1f%%',
+            explode=(0.1, 0.1, 0.1, 0.1, 0.1), labeldistance=1.05)
+    plt.axis('equal')
+    plt.title(' Fatality Composition, by Person Type: {}'.format(year))
+    plt.legend(labels, bbox_to_anchor=(1.7, 0.8), loc='center right')
+    plt.show()
+
+
+def states_shape_merge(df, year):
+
+    states_shp = geopandas.read_file('data/geopandas/usa-states-census-2014.shp')
+    states_shp = states_shp.merge(df[year], on = "NAME")
+    states_shp['fatalities{}'.format(year-1)] = states_shp['fatalities{}'.format(year-1)].str.replace(',', '').astype(int)
+    return states_shp
+
+def percent_change_plot(states_shp, col_name, year):
+    fig = plt.figure(1, figsize=(25,15))
+    ax = fig.add_subplot()
+    states_shp.apply(lambda x: ax.annotate(
+    s=x.STUSPS + "\n" + str(x["fatalities{}".format(year-1)]) + "\n" + str(x["{}{}".format(col_name, year)]) + "%",
+    xy=x.geometry.centroid.coords[0],
+    ha='center',
+    fontsize=15),axis=1);
+    states_shp.boundary.plot(ax=ax, color='Black', linewidth=.10)
+    states_shp.plot(ax=ax, cmap='Pastel2', figsize=(12, 12))
+    plt.title('Fatalities in {} and percent change from {} to {}'.format(year, year-1, year))
+
+def chloropleth(states_shp, col_name, year):
+    fig, ax = plt.subplots(1, figsize=(12,8))
+    states_shp.apply(lambda x: ax.annotate(
+    s=x.STUSPS ,
+    xy=x.geometry.centroid.coords[0],
+    ha='center',
+    fontsize=9),axis=1);
+    states_shp.plot(column='{}{}'.format(col_name, year), cmap='Blues', linewidth=1, ax=ax, edgecolor='0.9', legend = True)
+    plt.title('Fatalities in the USA in {}'.format(year))
+    ax.axis('off')
