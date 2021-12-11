626beee90827328d21f896f83042a38722c42408
priyankadewan99 <89533234+priyankadewan99@users.noreply.github.com>
2021-12-11 10:52:02 -0600

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import xlrd
-import openpyxl
+# import seaborn as sns
+# import xlrd
+# import openpyxl
 import geopandas
-from shapely import wkt
+# from shapely import wkt
+
 
 def read_file(year, filename):
-    dataframes_dict = {}
     file = "data/" + str(year) + "/" + str(filename) + ".csv"
     # file1 = "data/" + str(i) + "/ACCIDENT.csv"
     try:
@@ -18,22 +18,38 @@ def read_file(year, filename):
 
     return df
 
-def read_file_excel(year, filename, head = 0):
-    dataframes_dict = {}
+
+def read_file_excel(year, filename, head=0):
     file = "data/" + str(year) + "/" + str(filename) + ".xlsx"
     # file1 = "data/" + str(i) + "/ACCIDENT.csv"
     try:
-        df = pd.read_excel(file, header = head)
+        df = pd.read_excel(file, header=head)
     except:
-        df = pd.read_excel(file, header = head, encoding='unicode_escape')
+        df = pd.read_excel(file, header=head, encoding='unicode_escape')
 
     return df
+
+
+def bar_line_chart(df, graph_details):
+    x = []
+    for year in range(graph_details["start_year"], 2020, 2):
+        x.append(year)
+    plt.figure(1, figsize=(15, 10))
+    plt.title('{} and {} Rate per 100 Million VMT, {}-2019'.format(graph_details["ylabel"], graph_details["line_bar_key"],
+                                                                   graph_details["start_year"]))
+    barchart = plt.bar(df['Year'], df['Total {}'.format(graph_details["ylabel"])], color='green')
+    plt.xlabel(graph_details["xlabel"])
+    plt.ylabel(graph_details["ylabel"])
+    plt.twinx()
+    linechart = plt.plot(df['Year'], df['{} Rate per 100 Million VMT'.format(graph_details["line_bar_key"])], color='red')
+    plt.xticks(x)
+    plt.ylabel('{} Rate Per 100 Million VMT'.format(graph_details["line_bar_key"]))
 
 
 def values(df):
     total_killed = {}
     for index, row in df.iterrows():
-        if ((row['Vehicle Type'] != 'Total') and (row['Person Type'] == 'Subtotal')):
+        if (row['Vehicle Type'] != 'Total') and (row['Person Type'] == 'Subtotal'):
             total_killed[row['Vehicle Type']] = row['Occupants Killed']
         elif ((row['Vehicle Type'] == 'Motorcyclists' or row['Vehicle Type'] == 'Nonoccupants') and (
                 row['Person Type'] == 'Total')):
@@ -46,7 +62,6 @@ def values(df):
 def plotpie(dict1, year):
     labels = []
     number = []
-    percent = []
     plt.figure(1, figsize=(7, 7))
 
     # Referred from
@@ -65,29 +80,26 @@ def plotpie(dict1, year):
 def states_shape_merge(df, year):
 
     states_shp = geopandas.read_file('data/geopandas/usa-states-census-2014.shp')
-    states_shp = states_shp.merge(df[year], on = "NAME")
+    states_shp = states_shp.merge(df[year], on="NAME")
     states_shp['fatalities{}'.format(year-1)] = states_shp['fatalities{}'.format(year-1)].str.replace(',', '').astype(int)
     return states_shp
 
+
 def percent_change_plot(states_shp, col_name, year):
-    fig = plt.figure(1, figsize=(25,15))
+    fig = plt.figure(1, figsize=(25, 15))
     ax = fig.add_subplot()
     states_shp.apply(lambda x: ax.annotate(
     s=x.STUSPS + "\n" + str(x["fatalities{}".format(year-1)]) + "\n" + str(x["{}{}".format(col_name, year)]) + "%",
     xy=x.geometry.centroid.coords[0],
-    ha='center',
-    fontsize=15),axis=1);
+    ha='center', fontsize=15), axis=1)
     states_shp.boundary.plot(ax=ax, color='Black', linewidth=.10)
     states_shp.plot(ax=ax, cmap='Pastel2', figsize=(12, 12))
     plt.title('Fatalities in {} and percent change from {} to {}'.format(year, year-1, year))
 
+
 def chloropleth(states_shp, col_name, year):
-    fig, ax = plt.subplots(1, figsize=(12,8))
-    states_shp.apply(lambda x: ax.annotate(
-    s=x.STUSPS ,
-    xy=x.geometry.centroid.coords[0],
-    ha='center',
-    fontsize=9),axis=1);
-    states_shp.plot(column='{}{}'.format(col_name, year), cmap='Blues', linewidth=1, ax=ax, edgecolor='0.9', legend = True)
+    fig, ax = plt.subplots(1, figsize=(12, 8))
+    states_shp.apply(lambda x: ax.annotate(s=x.STUSPS, xy=x.geometry.centroid.coords[0], ha='center', fontsize=9), axis=1)
+    states_shp.plot(column='{}{}'.format(col_name, year), cmap='Blues', linewidth=1, ax=ax, edgecolor='0.9', legend=True)
     plt.title('Fatalities in the USA in {}'.format(year))
     ax.axis('off')
